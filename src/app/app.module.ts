998c3f8441ec5acb49cc77cd6be49a3f77f6b936
Shivan Prasad <68702232+shivanprasad@users.noreply.github.com>
2022-03-24 08:45:31 -0400

    feat: Implemented Firebase hosting functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CustomerbarComponent} from "./navigation/header/customerbar.component";
 import {MainMenuComponent} from "./navigation/header/main-menu.component";
-import {VideoComponent} from "./pages/video.component";
+import {VideoComponent} from "./store/video.component";
 import {BottomNavBarComponent} from "./navigation/footer/bottomnavbar.component";
-import {CarouselComponent} from "./pages/carousel.component";
-import {MotivationalLayoutComponent} from "./pages/motivational-layout.component";
-import {HomeLayoutComponent} from "./pages/home-layout.component";
-import {ArtistsLayoutComponent} from "./pages/artists-layout.component";
-import {KidsLayoutComponent} from "./pages/kids-layout.component";
-import {WomenArtistsLayoutComponent} from "./pages/women-artists-layout.component";
-import {MonopolyLayoutComponent} from "./pages/monopoly-layout.component";
-import {NBALegendsLayoutComponent} from "./pages/nba-legends-layout.component";
-import {AllCollectionsLayoutComponent} from "./pages/all-collections-layout.component";
-import {ExploreLayoutComponent} from "./pages/explore-layout.component";
-import {MuhammadAliLayoutComponent} from "./pages/muhammad-ali-layout.component";
-import {SmileyLayoutComponent} from "./pages/smiley-layout.component";
-import {PopeyeLayoutComponent} from "./pages/popeye-layout.component";
-import {ScrabbleLayoutComponent} from "./pages/scrabble-layout.component";
-import {BestSellersLayoutComponent} from "./pages/best-sellers-layout.component";
-import {PeanutsLayoutComponent} from "./pages/peanuts-layout.component";
-import {ProductCardComponent} from "./pages/product-card.component";
-import {BundlesLayoutComponent} from "./pages/bundles-layout.component";
-import {ProductGridComponent, ProductGridComponentBundles} from "./pages/product-grid.component";
+import {CarouselComponent} from "./store/carousel.component";
+import {MotivationalLayoutComponent} from "./store/motivational-layout.component";
+import {HomeLayoutComponent} from "./store/home-layout.component";
+import {ArtistsLayoutComponent} from "./store/artists-layout.component";
+import {KidsLayoutComponent} from "./store/kids-layout.component";
+import {WomenArtistsLayoutComponent} from "./store/women-artists-layout.component";
+import {MonopolyLayoutComponent} from "./store/monopoly-layout.component";
+import {NBALegendsLayoutComponent} from "./store/nba-legends-layout.component";
+import {AllCollectionsLayoutComponent} from "./store/all-collections-layout.component";
+import {ExploreLayoutComponent} from "./store/explore-layout.component";
+import {MuhammadAliLayoutComponent} from "./store/muhammad-ali-layout.component";
+import {SmileyLayoutComponent} from "./store/smiley-layout.component";
+import {PopeyeLayoutComponent} from "./store/popeye-layout.component";
+import {ScrabbleLayoutComponent} from "./store/scrabble-layout.component";
+import {BestSellersLayoutComponent} from "./store/best-sellers-layout.component";
+import {PeanutsLayoutComponent} from "./store/peanuts-layout.component";
+import {ProductCardComponent} from "./store/product-card.component";
+import {BundlesLayoutComponent} from "./store/bundles-layout.component";
+import {ProductGridComponent, ProductGridComponentBundles} from "./store/product-grid.component";
 import {HttpClientModule} from "@angular/common/http";
 import { UserInfoComponent } from './user-info/user-info.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 
 @NgModule({
@@ -59,6 +60,7 @@ import { UserInfoComponent } from './user-info/user-info.component';
         ProductGridComponent,
         ProductGridComponentBundles,
         UserInfoComponent,
+        AddProductComponent,
     ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,26 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBacCdppnnmf8otqJASAqT9Us-U0sOGZ3M",
+  authDomain: "ikonick-replica.firebaseapp.com",
+  databaseURL: "https://ikonick-replica-default-rtdb.firebaseio.com",
+  projectId: "ikonick-replica",
+  storageBucket: "ikonick-replica.appspot.com",
+  messagingSenderId: "816609963600",
+  appId: "1:816609963600:web:27372eb1c5be6812436b55",
+  measurementId: "G-39B3NMVVFF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
